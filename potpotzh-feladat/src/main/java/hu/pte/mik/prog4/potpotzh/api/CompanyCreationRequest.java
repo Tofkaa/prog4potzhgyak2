4bd46a0da656487e7928bab236defa1aa21af12f
Tofkaa <vortypresents@gmail.com>
2026-05-17 14:07:18 +0200

    creation request doesnt need id as db is autoincrement

--- potpotzh-feladat/src/main/java/hu/pte/mik/prog4/potpotzh/api/CompanyCreationRequest.java
+++ potpotzh-feladat/src/main/java/hu/pte/mik/prog4/potpotzh/api/CompanyCreationRequest.java
@@ -3,19 +3,19 @@ package hu.pte.mik.prog4.potpotzh.api;
 public class CompanyCreationRequest {
 
 
-    private Long id;
+    //private Long id;
     private String companyName;
     private Long estYear;
     private String country;
     private String knownProducts;
 
-    public Long getId() {
-        return id;
-    }
+    //public Long getId() {
+       // return id;
+    //}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    //public void setId(Long id) {
+      //  this.id = id;
+    //}
 
     public String getCompanyName() {
         return companyName;
